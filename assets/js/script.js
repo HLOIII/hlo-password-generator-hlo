f01bc2625d40fdd5ef3808fb9c9272b20c29aa44
Harry <chairmanharryogletree@gmail.com>
2021-12-19 02:00:11 -0500

    <write password section>

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,19 +12,33 @@ function writePassword() {
     // This will ask about the length of password
     var numCharacters = parseInt(window.prompt("How many characters would you like your password to contain?(between 8-128 characters):"));
 
-    // This checks for the length of requested password is at least 8 characters
+    // This makes sure that the length of requested password is at least 8 characters
     if (numCharacters < 8) {
         window.alert("Your password must be at least 8 characters");
         generatePassword();
     }
 
+    // This makes sure that the length of requested password is no more than 128 characters
+    else if (numCharacters > 128) {
+        window.alert("Your password must be no more than 128 characters");
+        generatePassword();
+    }
 
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+    // When requested password is between 8 to 128 characters
+    else {
+        var okSpecial = window.confirm("Click OK to confirm including special characters.");
+        if (okSpecial) {
+            var i = (Math.floor(Math.random) * 10) + 22
+            console.log(specialCharacters[i]);
+        }
 
-    passwordText.value = password;
 
-}
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+        passwordText.value = password;
+
+    }
+
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
