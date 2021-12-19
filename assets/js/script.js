461fe247826a57aed8751084e1b2607d0fd93d29
Harry <chairmanharryogletree@gmail.com>
2021-12-19 01:55:36 -0500

    <write password section>

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+    // This will ask about the length of password
+    var numCharacters = parseInt(window.prompt("How many characters would you like your password to contain?(between 8-128 characters):"));
+
+    // This checks for the length of requested password is at least 8 characters
+    if (numCharacters < 8) {
+        window.alert("Your password must be at least 8 characters");
+        generatePassword();
+    }
+
+
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
 
