21bb7c68c8c405c98135f91a405ba6f14ff5172e
Harry <chairmanharryogletree@gmail.com>
2021-12-19 21:02:04 -0500

    <fixing some errors>

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,26 +13,26 @@ var generatePassword = function () {
 
 
 
-    // Ask users about the length of password
+    // This ask users about the length of password
     var numCharacters = parseInt(window.prompt("How many characters would you like your password to contain?(between 8-128 characters):"));
 
-    // Check the length of requested password is at least 8 characters
+    // This will check to make sure the length of requested password is at least 8 characters
     if (numCharacters < 8) {
         window.alert("Your password must be at least 8 characters");
         generatePassword();
     }
 
-    // Check the length of requested password is no more than 128 characters
+    // This will check to make sure the length of requested password is no more than 128 characters
     else if (numCharacters > 128) {
         window.alert("Your password must be no more than 128 characters");
         generatePassword();
     }
 
-    // When requested password is between 8 to 128 characters
+    // Requested password is between 8 to 128 characters
     else if (numCharacters >= 8 && numCharacters <= 128) {
         var okSpecial = window.confirm("Click OK to confirm including special characters.");
         var okNumeric = window.confirm("Click OK to confirm including numeric values.");
-        var okUpperCase = window.confirm("Click OK to confirm including upprcase characters.");
+        var okUpperCase = window.confirm("Click OK to confirm including uppercase characters.");
         var okLowerCase = window.confirm("Click OK to confirm including lowercase characters.");
 
         var combinedArray = [];
@@ -58,9 +58,22 @@ var generatePassword = function () {
 
         if (okLowerCase) {
             var randomElement4 = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-
+            passwordGenerated = passwordGenerated + randomElement4;
+            combinedArray = combinedArray.concat(lowerCase);
         }
 
+        if (!okSpecial && !okNumeric && !okLowerCase && !okUpperCase) {
+            alert("At least one of the criteria should be selected. Please try again!");
+            writePassword();
+            return;
+        }
+
+        var passwordLength = numCharacters - passwordGenerated.length;
+        for (var i = 0; i < passwordLength; i++) {
+            passwordGenerated = passwordGenerated + combinedArray[Math.floor(Math.random() * combinedArray.length)];
+        }
+
+        return passwordGenerated;
     }
 
     else {
